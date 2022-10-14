c664bae48025658ae37dbaffc27f0b19c148c981
rahul <neorahul@outlook.in>
2022-10-14 22:20:24 +0530

    updated

--- checkPrimeN.cpp
+++ checkPrimeN.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
  int n;
- cout<<"enter number :\n";
+ cout<<"enter number : ";
  cin>>n;
  int i;
  if(n>=2){
        for(i=2;i<n;i++){
+        cout<<i<<endl;
           if(n%i==0){
               cout<<"number not prime "<<endl;
               break;
